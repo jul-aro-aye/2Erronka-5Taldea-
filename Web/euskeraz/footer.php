6d1bcf72a65a7028bd9a8869721d4956a8bf46dc
Alain-Afonso-Redondo <alainafonsoredondo@gmail.com>
2025-02-13 08:36:46 +0100

    Aldaketak ordaindu php orrian javascript jquery-ra bihurtu + euskeraz eta ingelesez karpetak egin

---
+++ Web/euskeraz/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer">
+        <div class="footer-content">
+            <h3>&#169; EkoTekno</h3>
+            <p><i class="fa fa-phone"></i> 943 345 678</p>
+            <p><i class="material-icons" style="font-size:13px;">place</i> San Esteban Auzoa,4</p>
+            <p><i class="fa fa-envelope"></i> ekoteknoinfo@ekotekno.com</p>
+        </div>
+
+    </div>
+</footer>
---
+++ Web/euskeraz/hizkuntzak/ingelesa.php
@@ -0,0 +1,18 @@
+<?php
+
+return{
+    "Zer aurkituko duzu EkoTeknon?" => "What will you find at EkoTekno?",
+    "Ordenagailuak eta osagarriak – Eramangarriak, mahaigaineko PC-ak, monitoreak, teklatuak eta gehiago." => "Computers and accessories - Laptops, desktop PCs, monitors, keyboards and more.",
+    "Smartphone-ak eta osagarriak – Azken modeloak, fundak, kargagailuak eta gailu adimendunak." => "Smartphones and accessories - Latest models, cases, chargers and smart devices.",
+    "Gadget-ak eta gailu adimendunak – Smartwatch-ak, entzungailuak, bozgorailu adimendunak eta domotika." => "Gadgets and smart devices - Smartwatches, headphones, smart speakers and home automation.",
+    "Osagaiak eta gaming-a – Txartel grafikoak, prozesadoreak, gaming-aulkiak eta zure esperientzia hobetzeko behar duzun guztia." => "Components and gaming - Graphics cards, processors, gaming chairs and everything you need to improve your experience.",
+    "Kalitatearekiko konpromisoa eta arreta pertsonalizatua" => "Commitment to quality and personalized attention",
+    
+    "EkoTekno-n, teknologiaren zaleak gara eta zerbitzu hurbila eta fidagarria eskaintzea gustatzen zaigu. 
+    Zure behar eta aurrekontura egokitutako produktua aurkitzen lagundukodizugu." 
+    => "At EkoTekno, we are technology enthusiasts and we like to offer fast and reliable service. 
+    We will help you find the product that best suits your needs and budget."
+
+
+    
+}
